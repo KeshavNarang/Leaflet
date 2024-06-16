e86c5473e8c70ddeaff050cddd5cbf7ab80366c5
Keshav Narang <56567680+KeshavNarang@users.noreply.github.com>
2024-06-15 17:57:15 -0700

    update app.py

--- app.py
+++ app.py
@@ -61,6 +61,8 @@ def login():
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
+    print(request.base_url)
+
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
         redirect_uri=request.base_url + "/callback",
